7b749c3792102994b603ac1d44e1cf266f0bfbdb
hamedfathi <hamedfathi@outlook.com>
2023-10-06 20:40:46 +0200

    feat(project): DefaultSecurityHeaders middleware added.

---
+++ HamedStack.AspNetCore.Security/Headers/DefaultSecurityHeaders.cs
@@ -0,0 +1,56 @@
+﻿// ReSharper disable UnusedType.Global
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HamedStack.AspNetCore.Security.Headers;
+
+/// <summary>
+/// Middleware to add default security headers to the HTTP response.
+/// </summary>
+public class DefaultSecurityHeaders
+{
+    private readonly RequestDelegate _next;
+    private readonly SecurityHeaderSettings _settings;
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DefaultSecurityHeaders"/> class.
+    /// </summary>
+    /// <param name="next">The next request delegate in the pipeline.</param>
+    /// <param name="settings">The security header settings.</param>
+    /// <param name="logger">The logger instance.</param>
+    public DefaultSecurityHeaders(RequestDelegate next, SecurityHeaderSettings settings, ILogger logger)
+    {
+        _next = next;
+        _settings = settings;
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Invokes the middleware.
+    /// </summary>
+    /// <param name="context">The HTTP context for the request.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task Invoke(HttpContext context)
+    {
+        _logger.LogInformation("Adding security headers");
+
+        // Add default headers
+        context.Response.Headers["X-Frame-Options"] = _settings.XFrameOptions;
+        context.Response.Headers["X-Permitted-Cross-Domain-Policies"] = _settings.XPermittedCrossDomainPolicies;
+        context.Response.Headers["X-Xss-Protection"] = _settings.XXssProtection;
+        context.Response.Headers["X-Content-Type-Options"] = _settings.XContentTypeOptions;
+        context.Response.Headers["Referrer-Policy"] = _settings.ReferrerPolicy;
+        context.Response.Headers["Permissions-Policy"] = _settings.PermissionsPolicy;
+        context.Response.Headers["Content-Security-Policy"] = _settings.ContentSecurityPolicy;
+
+        // Add custom headers
+        foreach (var header in _settings.CustomHeaders)
+        {
+            context.Response.Headers[header.Key] = header.Value;
+        }
+
+        await _next(context);
+    }
+}
---
+++ HamedStack.AspNetCore.Security/Headers/SecurityHeaderSettings.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable StringLiteralTypo
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable CollectionNeverUpdated.Global
+
+namespace HamedStack.AspNetCore.Security.Headers;
+
+/// <summary>
+/// Represents the security header settings for configuring response headers.
+/// </summary>
+public class SecurityHeaderSettings
+{
+    /// <summary>
+    /// Gets or sets the value for the X-Frame-Options header.
+    /// </summary>
+    public string XFrameOptions { get; set; } = "DENY";
+
+    /// <summary>
+    /// Gets or sets the value for the X-Permitted-Cross-Domain-Policies header.
+    /// </summary>
+    public string XPermittedCrossDomainPolicies { get; set; } = "none";
+
+    /// <summary>
+    /// Gets or sets the value for the X-Xss-Protection header.
+    /// </summary>
+    public string XXssProtection { get; set; } = "1; mode=block";
+
+    /// <summary>
+    /// Gets or sets the value for the X-Content-Type-Options header.
+    /// </summary>
+    public string XContentTypeOptions { get; set; } = "nosniff";
+
+    /// <summary>
+    /// Gets or sets the value for the Referrer-Policy header.
+    /// </summary>
+    public string ReferrerPolicy { get; set; } = "no-referrer";
+
+    /// <summary>
+    /// Gets or sets the value for the Permissions-Policy header.
+    /// </summary>
+    public string PermissionsPolicy { get; set; } = "camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), usb=()";
+
+    /// <summary>
+    /// Gets or sets the value for the Content-Security-Policy header.
+    /// </summary>
+    public string ContentSecurityPolicy { get; set; } = "default-src 'self'";
+
+    /// <summary>
+    /// Gets or sets the custom headers.
+    /// </summary>
+    public Dictionary<string, string> CustomHeaders { get; set; } = new();
+}
